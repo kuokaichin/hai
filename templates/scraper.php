e85192eaff6b04309609112770c49e65fc197dc1
Kuo-Kai Chin <kuokai.chin@gmail.com>
2012-11-26 21:26:46 -0500

    preg_match_all for id, activity name

--- templates/scraper.php
+++ templates/scraper.php
@@ -1,12 +1,24 @@
 <? 
-    // connect to database
-	$url = "http://usodb.fas.harvard.edu/public/index.cgi";
-	$html = file_get_contents($url);
-	
+    // get html from website    
+    $url = "http://usodb.fas.harvard.edu/public/index.cgi";
+    $html = file_get_contents($url);
+
+    // demarcate the beginning and end of portion we want to work with
     $html_start = '<ul>';
-    $html_start_pos = strpos($html, $html_start) + strlen($html_start);
-    $html_end = '</ul>';
-    $html = substr($html, $html_start_pos , strpos($html, $html_end, $html_start_pos) - $html_start_pos);
-    echo $html;
+        $html_start_pos = strpos($html, $html_start) + strlen($html_start);
+        $html_end = '</ul>';
+        $html = substr($html, $html_start_pos , strpos($html, $html_end, $html_start_pos) - $html_start_pos);
+
+    // make sure that we got the right substring
+    echo $html;    
+
+    // array to put scraped data
+    $activities = array();
+    preg_match_all('#(?<=id=)(\d*)#si', $html, $activities);
+    preg_match_all('#(?<=>)(.*?)(?=</)#si', $html, $activities);    
+    
+    print_r($activities);
 
 ?>
+
+
